e4cd43c33eea41bf69cdb74344bdda86ab054db7
Russ Trotter <rtrotter@intraedge.com>
2023-12-11 16:41:58 -0700

    day 11: remove the SPF algo for manhattan distance

--- src/main/java/net/flatball/aoc/D11.java
+++ src/main/java/net/flatball/aoc/D11.java
@@ -1,7 +1,7 @@
 package net.flatball.aoc;
 
-import java.util.*;
-import java.util.stream.Stream;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Felt pretty good about the approach on this one.  The impl started off rough, but a pass after getting the
@@ -11,6 +11,13 @@ import java.util.stream.Stream;
  * thing in the graph.  This was critical for part 2.  Probably more critical was the usage of a priority queue
  * for keeping adjacent edges managed during the SPF (thanks wikipedia!) algo to really make it fast.  It's one of those
  * cases where the linear search though a 2d array loses to a data structure.  Yay CS!
+ * <p>
+ * Edit: after reviewing some stuff on the subreddit, man, i kinda feel dumb.  Some fancy-pants SP algo is
+ * totally unnecessary since the "Manhattan Distance" is equivalent for this case.  I spent way too much time
+ * working that out and it was really for nothing.  Removed the SP function and just made a distance method
+ * that computes the manhattan distance and adjusts for the expansion using the same 2 arrays for horz/vert
+ * and it's crazy faster than what I had before.  There's still tons of optimizations that the real weens can do,
+ * but for me, but I'm happy with this update.
  */
 public class D11 implements AOC {
   record Vertex(int x, int y) {
@@ -22,58 +29,27 @@ public class D11 implements AOC {
   int[] horzCount;
   int[] vertCount;
 
+  int distance(Vertex src, Vertex dst, int weight) {
+    final int xMin = Math.min(src.x, dst.x);
+    final int xMax = Math.max(src.x, dst.x);
+    final int yMin = Math.min(src.y, dst.y);
+    final int yMax = Math.max(src.y, dst.y);
 
-  int dijkstra(Vertex src, Vertex dst, int weight) {
-    final int[][] dist = new int[height][width];
-    final PriorityQueue<Vertex> queue = new PriorityQueue<>(Comparator.comparingInt(v -> dist[v.y][v.x]));
-    for (int y = 0; y < height; y++) {
-      for (int x = 0; x < width; x++) {
-        dist[y][x] = Integer.MAX_VALUE;
+    int d = xMax - xMin;
+    for (int x = xMin + 1; x < xMax; x++) {
+      if (horzCount[x] == 0) {
+        d += (weight - 1);
       }
     }
-    // Distance of source vertex from itself is always 0
-    dist[src.y][src.x] = 0;
-    queue.add(src);
-
-    final int xMin = Math.max(0, Math.min(src.x, dst.x));
-    final int xMax = Math.min(width, Math.max(src.x, dst.x));
-    final int yMin = Math.max(0, Math.min(src.y, dst.y));
-    final int yMax = Math.min(height, Math.max(src.y, dst.y));
-
-
-    for (; ; ) {
-      if (queue.isEmpty()) {
-        throw new IllegalStateException("queue should not empty before finding dest");
+    d += (yMax - yMin);
+    for (int y = yMin + 1; y < yMax; y++) {
+      if (vertCount[y] == 0) {
+        d += (weight - 1);
       }
-      final Vertex vert = queue.remove();
-      if (vert.equals(dst)) {
-        return dist[vert.y][vert.x];
-      }
-
-      final Vertex up = vert.y > yMin ? new Vertex(vert.x, vert.y - 1) : null;
-      final Vertex right = vert.x < xMax ? new Vertex(vert.x + 1, vert.y) : null;
-      final Vertex down = vert.y < yMax ? new Vertex(vert.x, vert.y + 1) : null;
-      final Vertex left = vert.x > xMin ? new Vertex(vert.x - 1, vert.y) : null;
-
-      Stream.of(up, right, down, left).filter(Objects::nonNull).forEach(adj -> {
-        int w = 1;
-        if (adj.x != vert.x && horzCount[adj.x] == 0) {
-          w *= weight;
-        }
-        if (adj.y != vert.y && vertCount[adj.y] == 0) {
-          w *= weight;
-        }
-        int d = dist[vert.y][vert.x] + w;
-        final int adjDist = dist[adj.y][adj.x];
-        if (d < adjDist) {
-          dist[adj.y][adj.x] = d;
-          if (!queue.contains(adj)) {
-            queue.add(adj);
-          }
-        }
-      });
     }
+    return d;
   }
+
 
   @Override
   public void run(int part, List<String> lines) {
@@ -108,11 +84,12 @@ public class D11 implements AOC {
 
     long total = 0;
     for (int g = 0; g < galaxies.size(); g++) {
-      System.out.println("PROCESSING SRC " + (g + 1));
+      //System.out.println("PROCESSING SRC " + (g + 1));
       final Vertex from = galaxies.get(g);
       for (int to = g + 1; to < galaxies.size(); to++) {
         final Vertex dst = galaxies.get(to);
-        int d = dijkstra(from, dst, part == 2 ? 1000000 : 2);
+        int d = distance(from, dst, part == 2 ? 1000000 : 2);
+        //System.out.println("ASMONGLER " + (g + 1) + " to " + (to + 1) + "=" + d);
         total += d;
       }
     }
