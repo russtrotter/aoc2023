ba0f057429cbfea3d776b8cd9c9a28811ae8f16f
Russ Trotter <rtrotter@intraedge.com>
2023-12-16 09:50:08 -0700

    day 16: part 2

--- src/main/java/net/flatball/aoc/D16.java
+++ src/main/java/net/flatball/aoc/D16.java
@@ -1,11 +1,25 @@
 package net.flatball.aoc;
 
-import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
-import java.util.stream.IntStream;
+import java.util.Set;
 
+/**
+ * Another not-so-bad day.  Thought this one was gonna be P=NP after D15.  I'll take it though.  This one was fun.
+ * I think i kinda lucked into not falling into a brute-force trap by adding in cycle detection for encountering
+ * a grid location *in the same direction* thus short-circuiting recursion.  My early test runs
+ * were revealing infinite-loop behaviors on small data sets.  I don't know for sure because i had
+ * some early bugs in the 90-degree deflection pipes so maybe another case where carefully constructed test data
+ * had minimal cycles.  The primary dataset for sure hit the detection code so perhaps it just chugs longer if you
+ * don't put that detection in?
+ */
 public class D16 implements AOC {
   Grid grid;
+
+  record Key(int y, int x) {
+  }
+
+  final Set<Key> energized = new HashSet<>();
   Dir[][] intersect;
 
   @Override
@@ -13,23 +27,41 @@ public class D16 implements AOC {
     int result;
     this.grid = new Grid(lines);
     if (part == 1) {
-      intersect = new Dir[grid.dim][grid.dim];
-      Nav nav = new Nav();
-      beam(nav);
-      result = 0;
-      for (Dir[] dirs : intersect) {
-        for (Dir dir : dirs) {
-          result += dir == null ? 0 : 1;
+      result = cast(new Nav());
+    } else if (part == 2) {
+      result = Integer.MIN_VALUE;
+      for (int row = 0; row < grid.dim; row++) {
+        if (row == 0) {
+          // go down
+          for (int x = 0; x < grid.dim; x++) {
+            result = Math.max(result, cast(new Nav(row, x, Dir.DOWN)));
+          }
+        } else if (row == (grid.dim - 1)) {
+          // go up
+          for (int x = 0; x < grid.dim; x++) {
+            result = Math.max(result, cast(new Nav(row, x, Dir.UP)));
+          }
         }
+        // go right
+        result = Math.max(result, cast(new Nav(row, 0, Dir.RIGHT)));
+        // go left
+        result = Math.max(result, cast(new Nav(row, grid.dim - 1, Dir.LEFT)));
       }
     } else {
-      result = -1;
+      throw new IllegalStateException("bad part: " + part);
     }
     System.out.println("PART " + part + " result is " + result);
   }
 
+  int cast(Nav nav) {
+    intersect = new Dir[grid.dim][grid.dim];
+    energized.clear();
+    beam(nav);
+    return energized.size();
+  }
+
   void beam(Nav nav) {
-    for (;;) {
+    for (; ; ) {
       if (nav.x < 0 || nav.x >= grid.dim) {
         return;
       }
@@ -40,6 +72,8 @@ public class D16 implements AOC {
         return;
       }
       intersect[nav.y][nav.x] = nav.dir;
+      energized.add(new Key(nav.y, nav.x));
+
       switch (grid.data[nav.y][nav.x]) {
         case '.' -> nav.space();
         case '\\' -> {
@@ -120,6 +154,12 @@ public class D16 implements AOC {
     Nav() {
       x = y = 0;
       dir = Dir.RIGHT;
+    }
+
+    Nav(int y, int x, Dir dir) {
+      this.y = y;
+      this.x = x;
+      this.dir = dir;
     }
 
     Nav copy() {
