4e6fd21fa2f3eaf73c26fb5f4d0fd7e83edc94df
Russ Trotter <rtrotter@intraedge.com>
2023-12-21 16:26:53 -0700

    day 17: part 2

--- src/main/java/net/flatball/aoc/D17.java
+++ src/main/java/net/flatball/aoc/D17.java
@@ -2,11 +2,20 @@ package net.flatball.aoc;
 
 import java.util.*;
 
+/**
+ * Too many rabbit holes this day.  Had optimism in the early times, but kept beating
+ * my head against the wall and that stretched hours into days.  Finally succumbed and looked
+ * for a d17 hint and learned that i wasn't far off days prior.  Frustrating for sure.
+ * I can at least look back at a major lesson learned here and from prior days
+ * that a memoization strategy can make all the difference.  In this case, rather than a
+ * distance matrix based on a single point, ths problem requires a multi-dimensional distance
+ * matrix keyed by a tuple of (point, direction, streak) rather than by just a point as in the classic
+ * Dijkstra's algorithm.  I knew I had tried that at some point in my rabbit hole, but must have had
+ * some bug or some issue.  It's weird how once you have solid footing, the next part can be really easy.
+ * This particular p2 took about 10 additional minutes of dev and it was done.
+ */
 public class D17 implements AOC {
   int[][] weights;
-  boolean[][] visits;
-  int[][] costs;
-  int minPath;
   int dim;
 
 
@@ -32,12 +41,6 @@ public class D17 implements AOC {
         case RIGHT -> LEFT.equals(this);
       };
     }
-    boolean isHorizontal() {
-      return this == LEFT || this == RIGHT;
-    }
-    boolean isVertical() {
-      return this == UP || this == DOWN;
-    }
   }
 
   record Point(int y, int x) {
@@ -49,18 +52,14 @@ public class D17 implements AOC {
   @Override
   public void run(int part, List<String> lines) {
     grid(lines);
-    int result = 0;
-    if (part == 1) {
-      result = drive(new Point(0, 0), new Point(dim - 1, dim - 1));
-    }
+    int result = drive(part, new Point(0, 0), new Point(dim - 1, dim - 1));
     System.out.println("PART " + part + " is " + result);
   }
 
+  record Node(Point p, Dir dir, int streak) {
+  }
 
-  record Node(Point p, Dir dir, int streak) {}
-
-
-  int drive(Point location, Point destination) {
+  int drive(int part, Point location, Point destination) {
     final Map<Node, Integer> distances = new HashMap<>();
     final PriorityQueue<Node> stack = new PriorityQueue<>(dim * dim, Comparator.comparingInt(n -> distances.getOrDefault(n, Integer.MAX_VALUE)));
     final Node root = new Node(location, Dir.RIGHT, 0);
@@ -73,6 +72,8 @@ public class D17 implements AOC {
       if (pt.equals(destination)) {
         return distances.get(node);
       }
+      final List<Node> adjNodes = new ArrayList<>();
+
       for (Dir dir : Dir.values()) {
         final Point adj = pt.go(dir);
         if (adj.y < 0 || adj.y >= dim) {
@@ -84,21 +85,26 @@ public class D17 implements AOC {
         if (node.dir != null && dir.opposite(node.dir)) {
           continue;
         }
-        final int streak = dir.equals(node.dir) ? node.streak + 1 : 1;
-        if (streak > 3) {
+        if (part == 2 && node.streak < 4 && !dir.equals(node.dir)) {
           continue;
         }
-        final Node adjNode = new Node(adj, dir, streak);
+        final int streak = dir.equals(node.dir) ? node.streak + 1 : 1;
+        if ((part == 1 && streak > 3) || (streak > 10)) {
+          continue;
+        }
+        adjNodes.add(new Node(adj, dir, streak));
+      }
+
+
+      for (Node adjNode : adjNodes) {
         int ptd = distances.getOrDefault(node, Integer.MAX_VALUE);
         if (ptd == Integer.MAX_VALUE) {
           throw new IllegalStateException("wonk d");
         }
-        int d = ptd + weights[adj.y][adj.x];
+        int d = ptd + weights[adjNode.p.y][adjNode.p.x];
         if (d < distances.getOrDefault(adjNode, Integer.MAX_VALUE)) {
           distances.put(adjNode, d);
-          if (!stack.add(adjNode)) {
-            System.out.println("DUPE ADJ IN PQ");
-          }
+          stack.add(adjNode);
         }
       }
     }
--- src/test/java/net/flatball/aoc/D17Test.java
+++ src/test/java/net/flatball/aoc/D17Test.java
@@ -3,9 +3,6 @@ package net.flatball.aoc;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class D17Test extends AOCRunner {
   @Test
@@ -25,15 +22,16 @@ public class D17Test extends AOCRunner {
     process(new D17(), 2, 17, "_ex1");
   }
   @Test
+  public void part2ex2() throws IOException {
+    process(new D17(), 2, 17, "_ex2");
+  }
+  @Test
   public void part2() throws IOException {
     process(new D17(), 2, 17, "");
   }
 
   @Test
   public void tiny1() throws IOException {
-    D17 d = new D17();
-    d.grid(Util.lines("/d17/d17_tiny1.txt"));
-    d.drive(new D17.Point(0, 0), new D17.Point(0, 5));
-    //process(new D17(), 1, 17, "_tiny1");
+    process(new D17(), 1, 17, "_tiny1");
   }
 }
